dbc1b308239a50af6c5bc125b25942fa1cd596ae
Naman <naman.talaycha.cse17@itbhu.ac.in>
2021-05-04 14:02:33 +0530

    Intial Commit

---
+++ webpro/src/main/java/com/newpro/webpro/UserRepository.java
@@ -0,0 +1,9 @@
+package com.newpro.webpro;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+
+public interface UserRepository extends MongoRepository<User,String> {
+    
+}
---
+++ webpro/src/test/java/com/newpro/webpro/WebproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newpro.webpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
